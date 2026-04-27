40c63eb6454cfdadcb90189b3e2855910eca9012
SabaSiva2916 <sabasiva2916@gmail.com>
2026-04-27 19:59:19 +0530

    My First Code

---
+++ Sample.js
@@ -0,0 +1,15 @@
+let s = "Javascript";
+//To find the length of String
+console.log(s.length);
+
+//TO find the position of character
+console.log(s.charAt(3));
+console.log(s.charAt(11)); //We get empty String
+//In charAt() if we pass index Number greater than or equal to
+//length of String --> We get empty String
+
+//To convert the String into uppercase
+console.log(s.toUpperCase());
+
+//To convert the String into Lowercase
+console.log(s.toLowerCase());
